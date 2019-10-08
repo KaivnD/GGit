8b5922d18512a46caae30c4dee01ea97c97a2f15
KaivnD <KaivnD@hotmail.com>
2019-10-08 16:56:11 +0800

    feat:Canvas创建使挂上按钮、菜单和快捷键的钩子

--- GGit/GGitLoader.cs
+++ GGit/GGitLoader.cs
@@ -1,8 +1,11 @@
-﻿using Grasshopper;
+﻿using GGit.Forms;
+using Grasshopper;
 using Grasshopper.GUI;
 using Grasshopper.GUI.Canvas;
 using Grasshopper.Kernel;
+using LibGit2Sharp;
 using System;
+using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Windows.Forms;
@@ -16,32 +19,88 @@ namespace GGit
         {
         }
 
-        private bool _added;
+        private bool _btnAdded;
+
+        private bool _menuAdded;
 
         public override GH_LoadingInstruction PriorityLoad()
         {
-            CanvasCreated += AppendGGit;
+            CanvasCreated += AppendGGitBtn;
+            CanvasCreated += AppendGGitMenu;
             return GH_LoadingInstruction.Proceed;
         }
 
-        private void AppendGGit(GH_Canvas canvas)
+        private void AppendGGitBtn(GH_Canvas canvas)
         {
-            if (_added) return;
+            if (_btnAdded) return;
             foreach(Control control in canvas.Parent.Controls)
             {
                 var toolstrip = control as ToolStrip;
                 if (toolstrip != null)
                 {
-                    toolstrip.Items.Add("New Button", null, OnClick);
+                    toolstrip.Items.Add(null, Properties.Resources.git, OnClick);
                     return;
                 }
             }
-            _added = true;
+            _btnAdded = true;
+        }
+
+        private void AppendGGitMenu(GH_Canvas canvas)
+        {
+            if (_menuAdded) return;
+            foreach (Control ctrl in DocumentEditor.Controls)
+            {
+                var menuStrip = ctrl as MenuStrip;
+                if (menuStrip != null)
+                {
+                    ToolStripMenuItem customItem = new ToolStripMenuItem("GGit");
+                    ToolStripMenuItem init = new ToolStripMenuItem("Init", Properties.Resources.git, OnInitRepository);
+                    ToolStripMenuItem commit = new ToolStripMenuItem("Commit", Properties.Resources.git, OnDocumentCommit);
+                    ToolStripMenuItem push = new ToolStripMenuItem("Push", Properties.Resources.git, OnDocumentCommit);
+                    //init.Click += OnClick;
+                    commit.ShortcutKeys = Keys.Control | Keys.K;
+                    customItem.DropDown.Items.Add(init);
+                    customItem.DropDown.Items.Add(commit);
+                    customItem.DropDown.Items.Add(push);
+
+                    customItem.DropDown.Items.Add(new ToolStripSeparator());
+                    ToolStripItem toolStripItem4 = customItem.DropDown.Items.Add("NPKG", null, delegate
+                    {
+                        Process.Start("https://ncf.cz-studio.cn");
+                    });
+                    menuStrip.Items.Add(customItem);
+                    return;
+                }
+            }
+            _menuAdded = true;
+        }
+
+        private void OnInitRepository(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "Select a folder to be Initialize";
+            DialogResult res = folder.ShowDialog();
+            if (res == DialogResult.OK || res == DialogResult.Yes)
+            {
+                string repository = Repository.Init(@folder.SelectedPath);
+                if (!string.Equals(repository, string.Empty))
+                {
+                    MessageBox.Show(string.Format("初始化{0}成功", repository));
+                }
+            }
+        }
+
+        private void OnDocumentCommit(object sender, EventArgs e)
+        {
+            CommitForm commitForm = new CommitForm();
+            commitForm.Show();
         }
 
         private void OnClick(object sender, EventArgs e)
         {
-            MessageBox.Show("Haha", "lalsdf", MessageBoxButtons.OK);
+            string filepath = @"C:\Users\KaivnD\Desktop\ss.gh";
+            GGitViewer vv = new GGitViewer(filepath);
+            vv.FadeIn();
         }
     }
 }
