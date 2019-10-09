466a894d915342654e1c83708a5abc5de6f1a867
KaivnD <KaivnD@hotmail.com>
2019-10-09 17:27:52 +0800

    feat:增加列表来查看当前修改的文件，以及决定谁要暂存

--- GGit/Forms/CommitForm.Designer.cs
+++ GGit/Forms/CommitForm.Designer.cs
@@ -28,35 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.statusBox = new System.Windows.Forms.RichTextBox();
+            this.statusList = new BrightIdeasSoftware.ObjectListView();
+            this.olvColumn4 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn3 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            ((System.ComponentModel.ISupportInitialize)(this.statusList)).BeginInit();
             this.SuspendLayout();
             // 
-            // statusBox
+            // statusList
             // 
-            this.statusBox.Location = new System.Drawing.Point(12, 12);
-            this.statusBox.Name = "statusBox";
-            this.statusBox.ReadOnly = true;
-            this.statusBox.Size = new System.Drawing.Size(260, 142);
-            this.statusBox.TabIndex = 0;
-            this.statusBox.Text = "";
+            this.statusList.AllColumns.Add(this.olvColumn4);
+            this.statusList.AllColumns.Add(this.olvColumn1);
+            this.statusList.AllColumns.Add(this.olvColumn2);
+            this.statusList.AllColumns.Add(this.olvColumn3);
+            this.statusList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.statusList.CellEditUseWholeCell = false;
+            this.statusList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.olvColumn4,
+            this.olvColumn1,
+            this.olvColumn2,
+            this.olvColumn3});
+            this.statusList.Cursor = System.Windows.Forms.Cursors.Default;
+            this.statusList.Font = new System.Drawing.Font("微软雅黑 Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.statusList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.statusList.HideSelection = false;
+            this.statusList.Location = new System.Drawing.Point(0, 0);
+            this.statusList.Margin = new System.Windows.Forms.Padding(0);
+            this.statusList.Name = "statusList";
+            this.statusList.Size = new System.Drawing.Size(283, 200);
+            this.statusList.TabIndex = 1;
+            this.statusList.UseCompatibleStateImageBehavior = false;
+            this.statusList.View = System.Windows.Forms.View.Details;
+            // 
+            // olvColumn4
+            // 
+            this.olvColumn4.AspectName = "Status";
+            this.olvColumn4.IsVisible = false;
+            this.olvColumn4.Width = 25;
+            // 
+            // olvColumn1
+            // 
+            this.olvColumn1.AspectName = "Path";
+            this.olvColumn1.Groupable = false;
+            this.olvColumn1.IsEditable = false;
+            this.olvColumn1.Searchable = false;
+            this.olvColumn1.ShowTextInHeader = false;
+            this.olvColumn1.Sortable = false;
+            this.olvColumn1.Text = "File Path";
+            this.olvColumn1.Width = 150;
+            // 
+            // olvColumn2
+            // 
+            this.olvColumn2.AspectName = "Stage";
+            this.olvColumn2.ButtonPadding = new System.Drawing.Size(0, 0);
+            this.olvColumn2.ButtonSize = new System.Drawing.Size(25, 25);
+            this.olvColumn2.ButtonSizing = BrightIdeasSoftware.OLVColumn.ButtonSizingMode.CellBounds;
+            this.olvColumn2.Groupable = false;
+            this.olvColumn2.IsButton = true;
+            this.olvColumn2.IsEditable = false;
+            this.olvColumn2.ShowTextInHeader = false;
+            this.olvColumn2.Sortable = false;
+            this.olvColumn2.Text = "√";
+            this.olvColumn2.Width = 25;
+            // 
+            // olvColumn3
+            // 
+            this.olvColumn3.AspectName = "Discard";
+            this.olvColumn3.ButtonPadding = new System.Drawing.Size(0, 0);
+            this.olvColumn3.ButtonSize = new System.Drawing.Size(25, 25);
+            this.olvColumn3.ButtonSizing = BrightIdeasSoftware.OLVColumn.ButtonSizingMode.CellBounds;
+            this.olvColumn3.Groupable = false;
+            this.olvColumn3.IsButton = true;
+            this.olvColumn3.IsEditable = false;
+            this.olvColumn3.Searchable = false;
+            this.olvColumn3.ShowTextInHeader = false;
+            this.olvColumn3.Sortable = false;
+            this.olvColumn3.Text = "×";
+            this.olvColumn3.UseFiltering = false;
+            this.olvColumn3.Width = 25;
             // 
             // CommitForm
             // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Pane;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 461);
-            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.statusList);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "CommitForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "Commit";
+            ((System.ComponentModel.ISupportInitialize)(this.statusList)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        public System.Windows.Forms.RichTextBox statusBox;
+        private BrightIdeasSoftware.ObjectListView statusList;
+        private BrightIdeasSoftware.OLVColumn olvColumn1;
+        private BrightIdeasSoftware.OLVColumn olvColumn2;
+        private BrightIdeasSoftware.OLVColumn olvColumn3;
+        private BrightIdeasSoftware.OLVColumn olvColumn4;
     }
 }
---
+++ GGit/Utils/GStatus.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibGit2Sharp;
+
+namespace GGit.Utils
+{
+    public class GStatus
+    {
+        public string Path { get; set; }
+        public string Status { get; set; }
+        private int _status { get; set; }
+        public string Stage {get; set; }
+        public string Discard { get; set; }
+        
+        private string _workdir { get; set; }
+        public GStatus(string workdir, string path, int status)
+        {
+            _workdir = workdir;
+            Path = path;
+            _status = status;
+            Status = "Change";
+            SetBtn();
+        }
+
+        private void SetBtn()
+        {
+            Stage = "Staged" == Status ? "-" : "+";
+            Discard = "×";
+        }
+
+        public void StageFile()
+        {
+            try
+            {
+                using (var repo = new Repository(_workdir))
+                {
+                    Commands.Stage(repo, Path);
+                }
+                Status = "Staged";
+                SetBtn();
+            } catch (Exception err)
+            {
+                MessageBox.Show(string.Format(err.Message));
+            }
+        }
+
+        public void UnStageFile()
+        {
+            try
+            {
+                // TO DO Unstage a single file. git cli does this like command below
+                // git reset HEAD <file>
+                using (var repo = new Repository(_workdir))
+                {
+                    Commands.Unstage(repo, Path);
+                }
+                Status = "Change";
+                SetBtn();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(string.Format(err.Message));
+            }
+        }
+    }
+}
