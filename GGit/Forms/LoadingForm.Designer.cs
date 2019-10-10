a76c8f07c120089fdab3de63d7508ee5be424dcc
KaivnD <KaivnD@hotmail.com>
2019-10-10 15:59:13 +0800

    opt:修改loadingform的样式

--- GGit/Forms/LoadingForm.Designer.cs
+++ GGit/Forms/LoadingForm.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑 Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(38, 104);
+            this.label2.Location = new System.Drawing.Point(39, 104);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 17);
             this.label2.TabIndex = 1;
@@ -46,10 +46,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Image = global::GGit.Properties.Resources.Ball_1s_75px;
-            this.label1.Location = new System.Drawing.Point(39, 34);
+            this.label1.Location = new System.Drawing.Point(44, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 60);
-            this.label1.TabIndex = 0;
+            this.label1.TabIndex = 10;
             this.label1.Text = "         \r\n         \r\n         \r\n         \r\n         ";
             // 
             // LoadingForm
