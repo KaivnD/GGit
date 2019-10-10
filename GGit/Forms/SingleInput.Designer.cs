8cea62f6866677e061553011fa9f5d590de31121
KaivnD <KaivnD@hotmail.com>
2019-10-10 15:53:14 +0800

    opt:增加一个通用单输入框窗口

---
+++ GGit/Forms/SingleInput.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace GGit.Forms
+{
+    partial class SingleInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.singleLabel = new System.Windows.Forms.Label();
+            this.inputContent = new System.Windows.Forms.TextBox();
+            this.confirmBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // singleLabel
+            // 
+            this.singleLabel.AutoSize = true;
+            this.singleLabel.Location = new System.Drawing.Point(19, 34);
+            this.singleLabel.Name = "singleLabel";
+            this.singleLabel.Size = new System.Drawing.Size(41, 12);
+            this.singleLabel.TabIndex = 0;
+            this.singleLabel.Text = "label1";
+            // 
+            // inputContent
+            // 
+            this.inputContent.Location = new System.Drawing.Point(95, 31);
+            this.inputContent.Name = "inputContent";
+            this.inputContent.Size = new System.Drawing.Size(177, 21);
+            this.inputContent.TabIndex = 1;
+            // 
+            // confirmBtn
+            // 
+            this.confirmBtn.Location = new System.Drawing.Point(197, 76);
+            this.confirmBtn.Name = "confirmBtn";
+            this.confirmBtn.Size = new System.Drawing.Size(75, 23);
+            this.confirmBtn.TabIndex = 2;
+            this.confirmBtn.Text = "Confirm";
+            this.confirmBtn.UseVisualStyleBackColor = true;
+            this.confirmBtn.Click += new System.EventHandler(this.confirmBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(12, 76);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 3;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // SingleInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.confirmBtn);
+            this.Controls.Add(this.inputContent);
+            this.Controls.Add(this.singleLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SingleInput";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inputer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label singleLabel;
+        private System.Windows.Forms.TextBox inputContent;
+        private System.Windows.Forms.Button confirmBtn;
+        private System.Windows.Forms.Button cancelBtn;
+    }
+}
---
+++ GGit/Forms/SingleInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GGit.Forms
+{
+    public partial class SingleInput : Form
+    {
+        public delegate void ConfirmHandler(object sender, string input);
+        public event ConfirmHandler ConfirmEvent;
+        public SingleInput()
+        {
+            InitializeComponent();
+        }
+
+        private void confirmBtn_Click(object sender, EventArgs e)
+        {
+            if (inputContent.Text != "")
+            {
+                ConfirmEvent(this, inputContent.Text);
+            }
+            else MessageBox.Show("Input can't be empty.");
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
